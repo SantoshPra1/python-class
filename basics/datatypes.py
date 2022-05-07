93845cd3fb453b72b135fcffdb18c86cd6d41185
SantoshPra1 <104764094+SantoshPra1@users.noreply.github.com>
2022-05-07 17:03:52 +0530

    basic

---
+++ basics/datatypes.py
@@ -0,0 +1,12 @@
+a =5
+print(type(a))
+st = "santosh"
+print(type(str))
+list =[1,2,3]
+print(type(list))
+touple =(1,2,"ab")
+print(type(touple))
+set ={1,2,3,1,2,3}
+print(set,type(set))
+dic ={"apple":"fruit","potato" :"vegitable"}
+print(type(dic))
