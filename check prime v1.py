ced579f1bc71fb076b2b6fbb1aa8b432a8c62da3
SantoshPra1 <104764094+SantoshPra1@users.noreply.github.com>
2022-06-08 17:12:01 +0530

    string

---
+++ check prime v1.py
@@ -0,0 +1,8 @@
+def check_prime():
+    a=541
+    for i in range(2,541):
+        if a%i==0:
+            print('not prime')
+            break
+    else:
+        print('prime')
---
+++ functions/hello.py
@@ -0,0 +1,8 @@
+import random
+
+def say_hello():
+    msg =['Hello','Namaste','Hola','Bonjour','Guten tag']
+    print(random.choice(msg))
+
+
+say_hello()
