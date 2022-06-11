21c99f6157c2fda4068808312e330b70e08e1c01
SantoshPra1 <104764094+SantoshPra1@users.noreply.github.com>
2022-06-11 17:51:31 +0530

    map

---
+++ functions/lambda.py
@@ -0,0 +1,10 @@
+#f(x) = x + x - 3*5
+f = lambda x: x + x - 3 * 5
+print(f(3))
+print(f(2))
+print(f(10))
+
+
+g = lambda x,y: (x*y) -(x//y)
+
+print(g(3,4))
---
+++ functions/lambda3.py
@@ -0,0 +1,8 @@
+from ast import Lambda
+
+
+a =[1,2,3,4,5]
+b =[6,7,8,9,10]
+
+c =list(map(lambda i, j: i+j ,a,b))
+print(c)
